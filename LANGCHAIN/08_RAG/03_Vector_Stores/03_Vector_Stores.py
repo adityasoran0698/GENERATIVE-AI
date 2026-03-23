5e163588ab7ec0b03cf953ffeb76a430c5735955
Aditya Soran <adityasoran@gmail.com>
2026-03-24 00:15:49 +0530

    Uploaded!

--- LANGCHAIN/08_RAG/03_Vector_Stores/03_Vector_Stores.py
+++ LANGCHAIN/08_RAG/03_Vector_Stores/03_Vector_Stores.py
@@ -1,4 +1,11 @@
-from langchain_classic.schema import Document
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
 
 doc1 = Document(
     page_content="Virat Kohli is one of the best batsmen in the world. He has scored many centuries in international cricket and is known for his aggressive playing style.",
@@ -11,8 +18,8 @@ doc2 = Document(
 )
 
 doc3 = Document(
-    page_content="MS Dhoni is one of the most successful captains of India. He is known for his calm nature and finishing matches under pressure.",
-    metadata={"player": "MS Dhoni", "role": "Wicketkeeper", "id": 3},
+    page_content="MS Dhoni is one of the most successful captains of India in cricket. He is known for his calm nature and finishing matches under pressure.",
+    metadata={"player": "MS Dhoni", "role": "Wicketkeeper and Batsman", "id": 3},
 )
 
 doc4 = Document(
@@ -26,4 +33,34 @@ doc5 = Document(
 )
 
 docs = [doc1, doc2, doc3, doc4, doc5]
-print(docs)
+
+
+vector_store = Chroma(
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-large"),
+    persist_directory="chroma_db",
+    collection_name="sample",
+)
+vector_store.add_documents(docs)
+# result=vector_store.get(include=['embeddings','documents','metadatas'])
+query = input("You: ")
+result1 = vector_store.similarity_search(query=query,k=5)
+
+model = ChatOpenAI(model="gpt-4o-mini")
+
+prompt1 = PromptTemplate(
+    template="""You are an assistant. Answer ONLY from the given context.
+If the answer is not in the context, say "I don't know".
+
+Query: {query}
+
+Context:
+{document}
+"""
+)
+parser = StrOutputParser()
+context = "\n\n".join([doc.page_content for doc in result1])
+
+chain = prompt1 | model | parser
+result2 = chain.invoke({"query": query, "document": context})
+
+print(result2)
