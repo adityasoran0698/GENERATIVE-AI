367c735e33d03b1986de57b9b2105a4327f84d3c
Aditya Soran <adityasoran@gmail.com>
2026-03-28 21:16:09 +0530

    uploaded

---
+++ LANGCHAIN/09_Tools/01_Inbuilt_Tools.py
@@ -0,0 +1,11 @@
+from langchain_community.tools import DuckDuckGoSearchRun, ShellTool
+
+# search_tool = DuckDuckGoSearchRun()
+
+# result1 = search_tool.invoke("IPL 2026")
+# print(result1)
+
+shell_tool = ShellTool()
+
+result2 = shell_tool.invoke("")
+print(result2)
